e647dff11e5819d2a45f83fdcace0b1c9bda2aca
Hongyang Li <hongyangli2020@gmail.com>
2015-06-04 20:31:11 +0800

    disable the debug functionality of test net

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -154,7 +154,8 @@ void Solver<Dtype>::InitTestNets() {
     LOG(INFO)
         << "Creating test net (#" << i << ") specified by " << sources[i];
     test_nets_[i].reset(new Net<Dtype>(net_params[i]));
-    test_nets_[i]->set_debug_info(param_.debug_info());
+    //test_nets_[i]->set_debug_info(param_.debug_info());
+    test_nets_[i]->set_debug_info(false);
   }
 }
 
